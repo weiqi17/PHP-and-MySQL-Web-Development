1ac59b7fedfcf4c1daa84aa66be5019bc942b26b
Qi Wei <weiqi_0811@163.com>
2017-11-08 19:28:14 +0800

    用PHP生成的Bob运费表——while循环

---
+++ freight.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<table border="0" cellpadding="3">
+    <tr>
+        <td bgcolor="#cccccc" align="center">Distance</td>
+        <td bgcolor="#cccccc" align="center">Cost</td>
+    </tr>
+    <?php
+    $distance = 50;
+    while ($distance <= 250) {
+        echo "<tr>\n  <td align = 'right'>$distance</td>\n";
+        echo "  <td align = 'right'>". $distance / 10 ."</td>\n</tr>\n";
+        $distance += 50;
+    }
+    ?>
+</table>
+</body>
+</html>
